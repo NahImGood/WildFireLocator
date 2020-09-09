db3ee0d876e6435300f71e0de99197758390d4a2
Eli <Eli@ELI-PC>
2020-09-09 11:19:46 -0700

    New Fire Data, Adjusted PHP Scripting

--- PHP/return_json_from_file.php
+++ PHP/return_json_from_file.php
@@ -1,10 +1,9 @@
 <?php
 require 'functions.php';
-echo "Hello";
-// $fileName = $_GET['fileName'];
-// // $fileName = "J1_VIIRS_C2_USA_contiguous_and_Hawaii_VJ114IMGTDL_NRT_2020241";
-// $fireDataArray = convertCSVToArray("../firedata/FIRMS/noaa-20-viirs-c2/USA_contiguous_and_Hawaii/".$fileName.".txt");
-// // $jsonData = convertCSVToJSON($fireDataArray);
-// convertCSVToJSONAndSend($fireDataArray);
+$fileName = $_GET['fileName'];
+// $fileName = "J1_VIIRS_C2_USA_contiguous_and_Hawaii_VJ114IMGTDL_NRT_2020241";
+$fireDataArray = convertCSVToArray("../firedata/FIRMS/noaa-20-viirs-c2/USA_contiguous_and_Hawaii/".$fileName.".txt");
+// $jsonData = convertCSVToJSON($fireDataArray);
+convertCSVToJSONAndSend($fireDataArray);
 
 ?>
